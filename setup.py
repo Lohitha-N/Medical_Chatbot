4f0337be755ba84995e460c5e9f8fb22e30475e1
Lohitha <nlohitha9@gmail.com>
2025-04-08 00:04:51 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'N Lohitha',
+    author_email= 'nlohitha9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
